623d09c5102cb6f437fa0772969d0ecb811d6cf8
Matias Meza <153043791+MatSosa0@users.noreply.github.com>
2026-07-09 22:05:45 -0300

    Segunda-clase

---
+++ PrimerCurso/Funciones/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Funciones
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            Show();
+        }
+
+        static void Show()
+        {
+            Console.WriteLine("Hola soy un texto que se imprime desde funcion");
+        }
+    }
+}
