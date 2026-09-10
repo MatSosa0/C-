b59b0c47a94f3f612b0617b32a43907ff2ba2923
Matias Meza <153043791+MatSosa0@users.noreply.github.com>
2026-09-09 21:42:08 -0300

    Clase - static

---
+++ PrimerCurso/Static/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net.Cache;
+
+namespace Static
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            People matias = new People()
+            {
+                Name = "Matias",
+                Age = 28
+            };
+
+            People roberto = new People() {Name = "Roberto",Age = 35};
+
+            Console.WriteLine(People.count);
+            Console.WriteLine(People.GetCount()); 
+        }
+    }
+
+    public static class A
+    {
+        public static void Some()
+        {
+            Console.WriteLine("algo");
+        }
+    }
+
+    public class People
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public static int count = 0;
+
+        public People()
+        {
+            count++;
+        }
+
+        public static string GetCount()
+        {
+            return $"Esta clase se ha utilizado {count} veces";
+        }
+    }
+}
