0b795a19d1e9f2af250e701837b47d8c90b66107
Matias Meza <153043791+MatSosa0@users.noreply.github.com>
2026-09-10 22:26:03 -0300

    Clase - Interface

---
+++ PrimerCurso/Interface/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shark[] sharks = new Shark[] 
+            { 
+                new Shark("Espada", 45),
+                new Shark("Jaws", 65)
+            };
+
+            ShowFish(sharks);
+        }
+
+        public static void ShowFish(IFish[] fishs)
+        {
+            Console.WriteLine("-- Mostramos los peces --");
+
+            int i = 0;
+
+            while(i < fishs.Length)
+            {
+                Console.WriteLine(fishs[i].Swim());
+                i++;
+            }
+        }
+    }
+
+    public class Shark: IAnimal, IFish
+    {
+        public Shark(string Name, int Speed)
+        {
+            this.Name = Name;
+            this.Speed = Speed;
+        }
+        public string Name { get; set; }
+        public int Speed { get; set; }
+        public string Swim()
+        {
+            return $"{Name} nada a la velocidad {Speed} km/h";
+        }
+    }
+    public interface IAnimal
+    {
+        public string Name { get; set; }
+    }
+
+    public interface IFish
+    {
+        public int Speed { get; set; }
+        public string Swim();
+    }
+}
