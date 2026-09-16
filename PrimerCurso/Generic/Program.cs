4989eec0a971eaf7fd63a083da1c27c5573baf48
Matias Meza <153043791+MatSosa0@users.noreply.github.com>
2026-09-15 22:23:54 -0300

    clase - Generic - continuacion

--- PrimerCurso/Generic/Program.cs
+++ PrimerCurso/Generic/Program.cs
@@ -6,28 +6,42 @@ namespace Generic
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Cada vez me acuerdo mas de la sintaxis");
+            MyList<int> numbers = new MyList<int>(10);
+            numbers.Add(10);
+            numbers.Add(6);
+
+            MyList<string> strings = new MyList<string>(10);
+            strings.Add("Matias");
         }
     }
 
 
-    public class MyList
+    public class MyList<T>
     {
-        private int[] _elements;
+        private T[] _elements;
         private int _index = 0;
 
         public MyList(int n)
         {
-            _elements = new int[n];
+            _elements = new T[n];
         }
 
-        public void Add(int e)
+        public void Add(T e)
         {
             if(_index < _elements.Length)
             {
                 _elements[_index] = e;
                 _index++;
             }
+        }
+
+        public T GetElement(int i)
+        {
+            if(i <= _index && i >= 0)
+            {
+                return _elements[i];
+            }
+            return default(T);
         }
     }
 }
