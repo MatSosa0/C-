a4321779dad844101a86a45743e10a1b704e599e
Matias Meza <153043791+MatSosa0@users.noreply.github.com>
2026-09-16 21:39:54 -0300

    Clase - Generic

--- PrimerCurso/Generic/Program.cs
+++ PrimerCurso/Generic/Program.cs
@@ -12,9 +12,31 @@ namespace Generic
 
             MyList<string> strings = new MyList<string>(10);
             strings.Add("Matias");
+
+            MyList<People> people = new MyList<People>(5);
+            people.Add(new People() { Name = "Matias", Country = "Paraguay" });
+            people.Add(new People() { Name = "Ronaldo", Country = "Portugal" });
+
+            Console.WriteLine(people.GetString());
+
+            Console.WriteLine(numbers.GetString());
+            Console.WriteLine(strings.GetString());
+
+            Console.WriteLine(numbers.GetElement(11));
+            Console.WriteLine(strings.GetElement(0));
         }
     }
 
+    public class People 
+    {
+        public string Name { get; set; }
+        public string Country { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name: {Name} , Country: {Country}";
+        }
+    }
 
     public class MyList<T>
     {
@@ -42,6 +64,18 @@ namespace Generic
                 return _elements[i];
             }
             return default(T);
+        }
+
+        public string GetString()
+        {
+            int i = 0;
+            string result = "";
+            while(i < _index)
+            {
+                result += _elements[i].ToString() + "|";
+                i++;
+            }
+            return result;
         }
     }
 }
--- PrimerCurso/Generic/obj/Debug/net10.0/Generic.AssemblyInfo.cs
+++ PrimerCurso/Generic/obj/Debug/net10.0/Generic.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Generic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+831db1f97509c625c0b6d35ddfb83f136f9bc106")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4989eec0a971eaf7fd63a083da1c27c5573baf48")]
 [assembly: System.Reflection.AssemblyProductAttribute("Generic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Generic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
