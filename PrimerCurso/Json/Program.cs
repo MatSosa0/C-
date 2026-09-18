bc1bf96f42407bc028b2681f325ce21ec35be3be
Matias Meza <153043791+MatSosa0@users.noreply.github.com>
2026-09-17 22:51:16 -0300

    Clase - Json

--- PrimerCurso/Json/Program.cs
+++ PrimerCurso/Json/Program.cs
@@ -6,7 +6,33 @@ namespace Json
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Inicio - clase - Json");
+            Beer MyBeer = new Beer() { Name = "Pikantus", Brand = "Erdinger" };
+
+            string json = "{\"Name\": \"Pikantus\",\"Brand \": \"Erdinger\"}";
+
+            Beer[] beers = new Beer[]
+            {
+                new Beer()
+                {
+                    Name = "Pikantus",
+                    Brand = "Erdinger"
+                },
+                new Beer()
+                {
+                    Name = "Corona",
+                    Brand = "Modelo"
+                }
+            };
+
+            string json2 = "[" +
+                "{\"Name\": \"Pikantus\",\"Brand \": \"Erdinger\"}," +
+                "{\"Name\": \"Corona\",\"Brand \": \"Modelo\"}]";
+        }
+
+        public class Beer
+        {
+            public string Name { get; set; }
+            public string Brand { get; set; }
         }
     }
 }
