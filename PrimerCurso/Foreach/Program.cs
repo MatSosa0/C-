e33b8743be37e4f54fce9ceec2a345e5ad7f1c67
Matias Meza <153043791+MatSosa0@users.noreply.github.com>
2026-09-22 21:04:56 -0300

    Clase - Foreach

---
+++ PrimerCurso/Foreach/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>
+            {
+                1,2,34,5,67,78
+            };
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
