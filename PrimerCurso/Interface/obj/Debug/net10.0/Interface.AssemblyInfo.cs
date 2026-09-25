f4a595f64d7ff661422d997e52d0cf21aad99121
Matias Meza <153043791+MatSosa0@users.noreply.github.com>
2026-09-24 21:12:55 -0300

    Clase Metodos de Listas

--- PrimerCurso/Interface/obj/Debug/net10.0/Interface.AssemblyInfo.cs
+++ PrimerCurso/Interface/obj/Debug/net10.0/Interface.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Interface")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36d925385d8cfd4285c59b76b745cd2885249d81")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d59d41ce1570813ada9df30970a67ffe4770bc67")]
 [assembly: System.Reflection.AssemblyProductAttribute("Interface")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Interface")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- PrimerCurso/Static/obj/Debug/net10.0/Static.AssemblyInfo.cs
+++ PrimerCurso/Static/obj/Debug/net10.0/Static.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Static")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36d925385d8cfd4285c59b76b745cd2885249d81")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d59d41ce1570813ada9df30970a67ffe4770bc67")]
 [assembly: System.Reflection.AssemblyProductAttribute("Static")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Static")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
